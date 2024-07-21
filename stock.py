08625e1ffefcffa3597ee955080b070f92eb772e
iking919 <153115930+iking919@users.noreply.github.com>
2024-07-21 17:33:52 -0400

    Total change
    stocks total change (original price - current price)

--- stock.py
+++ stock.py
@@ -9,6 +9,8 @@ class Stock:
         self.risklvl = risklvl
         self.potreturn = potreturn
         self.marketinf = marketinf
+        self.start = price
+        self.totchange = None
         self.change = None
         self.mu = mu        # Drift coefficient
         self.sigma = sigma  # Volatility coefficient
@@ -16,6 +18,7 @@ class Stock:
         self.owned = 0
         self.needed = needed
         self.marketcap = marketcap
+        
     
     def fluctuate(self):
         tmp = self.price
@@ -23,14 +26,16 @@ class Stock:
         W = np.random.standard_normal()
         # Update the stock price using GBM formula
         self.price = self.price * np.exp((self.mu - 0.5 * self.sigma**2) * self.dt + self.sigma * np.sqrt(self.dt) * W)
-        self.change = self.price - tmp
+        self.change = ((self.price - tmp)/tmp) * 100
         self.price = round(float(self.price), 2)
+        self.totchange = ((self.price - self.start)/self.start) * 100
     
     def printinfo(self):
         print(f"\nName: {self.name}")
         print(f"Ticker: {self.ticker}")
         print(f"Price: {self.price:.2f}")
-        print(f"Change:{self.change:.2f}")
+        print(f"Daily change: {self.change:.2f}%")
+        print(f"Total net change: {self.totchange:.2f}%")
         print(f"Current shares owned: {self.owned}")
         print(f"Shares needed: {self.needed}")
         print(f"Market cap: {self.marketcap} \n")
